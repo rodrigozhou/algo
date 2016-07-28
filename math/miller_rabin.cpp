08fb5716454e46d21e1382901c9ecb4dd24bf87d
rodrigozhou <rodrigo.zhou@gmail.com>
2016-07-28 01:19:50 -0300

    miller rabin updated

--- math/miller_rabin.cpp
+++ math/miller_rabin.cpp
@@ -1,44 +1,52 @@
 /**
  * Miller-Rabin primality test
  *
- * This implementation is deterministic for n < 4,759,123,141
+ * This implementation is deterministic for n < 3,825,123,056,546,413,051
  */
 
-#include <cstdio>
-
-int powmod(int a, int e, int n) {
-    int ret = 1;
-    for (long long pow = a; e > 0; e >>= 1, pow = pow * pow % n)
-        if (e & 1)
-            ret = ret * pow % n;
+long long multmod(long long a, long long b, long long mod) {
+    long long ret = 0;
+    int msb = 64 - __builtin_clzll(b);
+    for (int bit = msb-1; bit >= 0; bit--) {
+        ret <<= 1;
+        if (ret >= mod) ret -= mod;
+        if (b >> bit & 1) {
+            ret += a;
+            if (ret >= mod) ret -= mod;
+        }
+    }
     return ret;
 }
 
-int miller_rabin(int n, int a) {
+long long powmod(long long a, long long e, long long mod) {
+    long long ret = 1;
+    for (a %= mod; e > 0; e >>= 1, a = multmod(a, a, mod))
+        if (e & 1)
+            ret = multmod(ret, a, mod);
+    return ret;
+}
+
+int miller_rabin(long long n, int a) {
     if (n <= 1) return 0;
-    if (!(n & 1)) return n == 2;
-
-    int d = n - 1, s = 0;
-    while (!(d & 1)) {
-        d >>= 1;
-        s++;
-    }
-
-    int x = powmod(a, d, n);
+    if (n % 2 == 0) return n == 2;
+    long long s = __builtin_ctzll(n-1), d = (n-1) >> s;
+    long long x = powmod(a, d, n);
     if (x == 1) return 1;
-
     for (int i = 0; i < s; i++) {
         if (x == 1) return 0;
         if (x == n-1) return 1;
-        x = (long long)x * x % n;
+        x = multmod(x, x, n);
     }
-
     return 0;
 }
 
-int isprime(int n) {
-    if (n == 2 || n == 7 || n == 61) return 1;
-    return miller_rabin(n, 2) && miller_rabin(n, 7) && miller_rabin(n, 61);
+int isprime(long long n) {
+    const int a[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+    for (int i = 0; i < 9; i++) {
+        if (n == a[i]) return 1;
+        if (!miller_rabin(n, a[i])) return 0;
+    }
+    return 1;
 }
 
 int main() {
