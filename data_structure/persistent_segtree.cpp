d5530e6904d51a270af11a6f2b25a8a82d59a054
rodrigozhou <rodrigo.zhou@gmail.com>
2016-11-20 21:18:42 -0200

    persistent segtree without pointers

--- data_structure/persistent_segtree.cpp
+++ data_structure/persistent_segtree.cpp
@@ -1,132 +1,61 @@
 /**
  * Persistent Segment Tree
  *
- * Problems: SPOJ/COT
+ * Problems: SPOJ/MKTHNUM, SPOJ/COT
  */
 
 #include <cstdio>
-#include <cstdlib>
-#include <cctype>
-#include <cstring>
 #include <algorithm>
-#include <vector>
 using namespace std;
 
 #define MAX 100005
-#define LOGMAX 18
-
-int n, val[MAX];
-int aux[MAX], dict[MAX], rdict[MAX], maxval;
-vector<int> g[MAX];
-int anc[MAX][LOGMAX], dad[MAX], lvl[MAX];
 
 struct Node {
-    int count;
-    Node *left, *right;
-
-    Node(int count, Node *left, Node *right)
-        : count(count), left(left), right(right) {}
-
-    Node *insert(int l, int r, int w);
+    int cnt, l, r;
+    Node () : cnt(0), l(0), r(0) {}
+    Node (int cnt, int l, int r) : cnt(cnt), l(l), r(r) {}
 };
 
-Node *nullNode = new Node(0, NULL, NULL);
+int n, a[MAX], b[MAX];
+Node tree[20*MAX];
+int root[MAX], id = 1;
 
-Node* Node::insert(int l, int r, int w) {
-    if (l <= w && w < r) {
-        if (r - l == 1)
-            return new Node(this->count + 1, nullNode, nullNode);
-        int m = (l + r) / 2;
-        return new Node(this->count + 1,
-                        this->left->insert(l, m, w),
-                        this->right->insert(m, r, w));
+int update(int no, int l, int r, int x) {
+    if (x < l || x > r) return no;
+    if (l == r) {
+        tree[id] = Node(1, 0, 0);
+        return id++;
     }
-    return this;
-}
-
-Node *root[MAX];
-
-void dfs(int u, int p) {
-    dad[u] = p;
-    lvl[u] = p == -1 ? 0 : lvl[p] + 1;
-
-    root[u] = (p == -1 ? nullNode : root[p])->insert(0, maxval, dict[u]);
-
-    for (int i = 0; i < g[u].size(); i++)
-        if (g[u][i] != p)
-            dfs(g[u][i], u);
-}
-
-void prelca() {
-    memset(anc, -1, sizeof(anc));
-    for (int i = 0; i < n; i++)
-        anc[i][0] = dad[i];
-    for (int j = 1; 1<<j < n; j++)
-        for (int i = 0; i < n; i++)
-            if (anc[i][j-1] != -1)
-                anc[i][j] = anc[anc[i][j-1]][j-1];
-}
-
-int lca(int u, int v) {
-    if (lvl[u] < lvl[v])
-        swap(u, v);
-    int log;
-    for (log = 1; 1<<log <= lvl[u]; log++);
-    log--;
-    for (int i = log; i >= 0; i--)
-        if (lvl[u] - (1 << i) >= lvl[v])
-            u = anc[u][i];
-    if (u == v)
-        return u;
-    for (int i = log; i >= 0; i--)
-        if (anc[u][i] != anc[v][i])
-            u = anc[u][i], v = anc[v][i];
-    return dad[u];
-}
-
-int query(Node *a, Node *b, Node *c, Node *d, int l, int r, int k) {
-    if (r - l == 1)
-        return l;
-    int count = a->left->count + b->left->count - c->left->count - d->left->count;
     int m = (l + r) / 2;
-    if (count >= k)
-        return query(a->left, b->left, c->left, d->left, l, m, k);
-    return query(a->right, b->right, c->right, d->right, m, r, k - count);
+    int ll = update(tree[no].l, l, m, x);
+    int rr = update(tree[no].r, m+1, r, x);
+    tree[id] = Node(tree[ll].cnt + tree[rr].cnt, ll, rr);
+    return id++;
+}
+
+int query(int a, int b, int l, int r, int k) {
+    if (l == r) return l;
+    int m = (l + r) / 2;
+    int cnt = tree[tree[b].l].cnt - tree[tree[a].l].cnt;
+    if (k <= cnt)
+        return query(tree[a].l, tree[b].l, l, m, k);
+    return query(tree[a].r, tree[b].r, m+1, r, k-cnt);
 }
 
 int main() {
-    int q;
+    int q, l, r, k;
     scanf("%d %d", &n, &q);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &val[i]);
-        aux[i] = val[i];
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+        b[i] = a[i];
     }
-
-    sort(aux, aux+n);
-    maxval = unique(aux, aux+n) - aux;
-    for (int i = 0; i < n; i++) {
-        dict[i] = lower_bound(aux, aux+maxval, val[i]) - aux;
-        rdict[dict[i]] = i;
+    sort(b+1, b+n+1);
+    for (int i = 1; i <= n; i++) {
+        a[i] = lower_bound(b+1, b+n+1, a[i]) - b;
+        root[i] = update(root[i-1], 1, n, a[i]);
     }
-
-    for (int i = 1; i < n; i++) {
-        int u, v;
-        scanf("%d %d", &u, &v);
-        u--, v--;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
-
-    nullNode->left = nullNode->right = nullNode;
-    dfs(0, -1);
-    prelca();
-
     while (q--) {
-        int u, v, k;
-        scanf("%d %d %d", &u, &v, &k);
-        u--, v--;
-        int w = lca(u, v);
-        int ans = query(root[u], root[v], root[w], dad[w] == -1 ? nullNode : root[dad[w]], 0, maxval, k);
-        printf("%d\n", val[rdict[ans]]);
+        scanf("%d %d %d", &l, &r, &k);
+        printf("%d\n", b[query(root[l-1], root[r], 1, n, k)]);
     }
 }
